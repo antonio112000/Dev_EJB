683a194e9b623b05c3c3dbb8c2d998856bdbba1c
antonio112000 <anto11.n@hotmail.com>
2022-03-27 21:26:04 +0300

    Initial commit

---
+++ src/main/java/com/example/dev_ejb/HelloResource.java
@@ -0,0 +1,15 @@
+package com.example.dev_ejb;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ejb.*;
+@Stateless
+@Path("/hello-world")
+public class HelloResource {
+    @GET
+    @Produces("text/plain")
+    public String hello() {
+        return "Hello EJB";
+    }
+}
---
+++ src/main/java/com/example/dev_ejb/HelloServlet.java
@@ -0,0 +1,28 @@
+package com.example.dev_ejb;
+
+import java.io.*;
+import javax.servlet.http.*;
+import javax.servlet.annotation.*;
+
+@WebServlet(name = "helloServlet", value = "/hello-servlet")
+public class HelloServlet extends HttpServlet {
+    private String message;
+    private HelloResource hr = new HelloResource();
+
+    public void init() {
+        message = hr.hello();
+    }
+
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        response.setContentType("text/html");
+
+        // Hello
+        PrintWriter out = response.getWriter();
+        out.println("<html><body>");
+        out.println("<h1>" + message + "</h1>");
+        out.println("</body></html>");
+    }
+
+    public void destroy() {
+    }
+}
